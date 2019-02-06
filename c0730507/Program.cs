106c024e0e2a370f224693c3de875116a23f4a4b
visualstudio <visualstudio@122-PC26>
2019-02-06 10:27:07 -0500

    stas

--- c0730507/Program.cs
+++ c0730507/Program.cs
@@ -8,8 +8,31 @@ namespace c0730507
 {
     class Program
     {
+        static int i = 0;
         static void Main(string[] args)
         {
+            MethodA();
+            Console.ReadLine();
+
+        }
+
+        public static void MethodA()
+        {
+            while (Peanut())
+            {
+
+                Console.WriteLine("i is {0}", i);
+
+                if (i > 10)
+                {
+                    return;
+                }
+            }
+        }
+        public static bool Peanut()
+        {
+            i++;
+            return true;
         }
     }
 }
